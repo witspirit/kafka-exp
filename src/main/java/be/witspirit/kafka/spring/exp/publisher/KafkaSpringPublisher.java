8ac76de605205cad09535d8636223de34505d8a4
witspirit <witspirit@gmail.com>
2019-04-14 20:07:02 +0200

    Added the setting of a 'key' and increased partitions to 2 to see what happens to the work distribution with multiple consumers in the same consumer group.

--- src/main/java/be/witspirit/kafka/spring/exp/publisher/KafkaSpringPublisher.java
+++ src/main/java/be/witspirit/kafka/spring/exp/publisher/KafkaSpringPublisher.java
@@ -1,20 +1,19 @@
 package be.witspirit.kafka.spring.exp.publisher;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.function.SupplierUtils;
 
 import java.time.Instant;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class KafkaSpringPublisher implements CommandLineRunner {
@@ -24,17 +23,30 @@ public class KafkaSpringPublisher implements CommandLineRunner {
         SpringApplication.run(KafkaSpringPublisher.class, args);
     }
 
+    @Bean
+    public NewTopic partUpdates() {
+        return new NewTopic("partUpdates", 20, (short) 1);
+    }
+
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+
     @Override
-    public void run(String... args) throws Exception {
-        for (int i=0; i < 1000; i++) {
-            ListenableFuture<SendResult<String, String>> partUpdates = kafkaTemplate.send("partUpdates", "partA-"+i+"@" + Instant.now());
-            // Instead of waiting using a get, this could be done async and then messages could continue to be produced while we await confirmation from the broker.
-            // For this tiny experiment, it's probably easier to KISS
-            SendResult<String, String> sendResult = partUpdates.get();
-            LOG.info("SendResult: " + sendResult);
+    public void run(String... args) throws ExecutionException, InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            for (String partId : new String[]{"A", "B", "C", "D", "E", "F", "G"}) {
+
+                String message = String.format("part%s-%4d@%s", partId, i, Instant.now());
+                String key = partId; // Will be used to consistently assign a key to the same partition
+                ListenableFuture<SendResult<String, String>> partUpdates = kafkaTemplate.send("partUpdates", key, message);
+                // Instead of waiting using a get, this could be done async and then messages could continue to be produced while we await confirmation from the broker.
+                // For this tiny experiment, it's probably easier to KISS
+                SendResult<String, String> sendResult = partUpdates.get();
+                LOG.info("SendResult: " + sendResult);
+
+            }
         }
     }
 }
